dafde94c2fe319b1af229db1bd1a7df1092a871d
andrew <uandrew1965@gmail.com>
2017-10-25 10:24:53 +0300

    .

--- project/CommonDeps.scala
+++ project/CommonDeps.scala
@@ -3,15 +3,13 @@ package ru.simplesys.sbprocessing.sbtbuild
 import sbt._
 
 object PluginDeps {
-    object versions {
-        val sbtNativePackagerVersion = "1.2.4"
-        val sbtResolverVersion = "0.9.0.1"
-        val jrabelPluginVersion = "0.11.1"
-    }
+    val sbtNativePackagerVersion = "1.2.4"
+    val sbtResolverVersion = "0.9.0.1"
+    val jrabelPluginVersion = "0.11.1"
 
-    val sbtNativePackager = addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % versions.sbtNativePackagerVersion)
-    val sbtRevolver = addSbtPlugin("io.spray" % "sbt-revolver" % versions.sbtResolverVersion)
-    val jrebelPlugin = addSbtPlugin("com.simplesys" % "jrebel-plugin" % versions.jrabelPluginVersion)
+    val sbtNativePackager = addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % sbtNativePackagerVersion)
+    val sbtRevolver = addSbtPlugin("io.spray" % "sbt-revolver" % sbtResolverVersion)
+    val jrebelPlugin = addSbtPlugin("com.simplesys" % "jrebel-plugin" % jrabelPluginVersion)
 }
 
 object CommonDeps {
